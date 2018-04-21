fcd5fe24d4ddd2eabbff7954f0ec57737da665b7
oscar0812 <oscar0812torres@gmail.com>
2018-04-21 18:06:06 -0500

    brought bot to life

--- src/BotHandler.java
+++ src/BotHandler.java
@@ -1,7 +1,28 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class BotHandler extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            // valid
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                SendMessage sendMessageRequest = new SendMessage();
+                sendMessageRequest.setChatId(message.getChatId().toString());
+                sendMessageRequest.setText("you said: " + message.getText());
+                try {
+                    execute(sendMessageRequest);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                } //end catch()
+            } //end if()
+        }
+    }
 
     @Override
     public String getBotUsername() {
@@ -9,14 +30,7 @@ public class BotHandler extends TelegramLongPollingBot {
     }
 
     @Override
-    public void onUpdateReceived(Update arg0) {
-        // TODO Auto-generated method stub
-        System.out.println("here");
-    }
-
-    @Override
     public String getBotToken() {
         return "508112557:AAFOC-R-KXsFniSQSVmGOBT4iSRMWMx2URA";
     }
-
 }
