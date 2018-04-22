263f6c4969941ea9e117a298c2c7ba81e4f16b53
LLLBitLLL <cwmversion4@gmail.com>
2018-04-21 18:12:11 -0700

    How much wood can a wood chuck chuck if a wood chuck could chuck wood?

--- src/BotHandler.java
+++ src/BotHandler.java
@@ -5,25 +5,55 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class BotHandler extends TelegramLongPollingBot {
+    private String rmsg,arg,cmd,username;
+    private Message message;
+    private boolean echo = false;
 
     @Override
     public void onUpdateReceived(Update update) {
+        
         if (update.hasMessage()) {
-            // valid
-            Message message = update.getMessage();
+            message = update.getMessage();
+
             if (message.hasText()) {
-                SendMessage sendMessageRequest = new SendMessage();
-                sendMessageRequest.setChatId(message.getChatId().toString());
-                sendMessageRequest.setText("you said: " + message.getText());
-                try {
-                    execute(sendMessageRequest);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                } //end catch()
-            } //end if()
+                rmsg = message.getText().toString();
+                username = message.getFrom().getUserName().toString();
+                System.out.println(username+":" +rmsg);
+                if (echo) {
+                    sendMessage(username +": " + rmsg);
+                }
+
+                // COMMAND DOSEN'T TAKE ARGUMENT
+                if(rmsg.contains("/") && !rmsg.contains(" ")){
+                    cmd = message.getText().toString().split("/")[1];
+
+                    if(cmd.equalsIgnoreCase("test")) {
+                        sendMessage("bbc pornos");
+                    } else if (cmd.equalsIgnoreCase("echo") && !echo) {
+                        echo = true;
+                        sendMessage("Echo is Enabled!");
+
+                    } else if (cmd.equalsIgnoreCase("echo off")){
+                        echo = false;
+                    }
+
+
+
+                    // COMMAND TAKES ARGUMENT
+                } else if (rmsg.startsWith("/") && rmsg.contains(" ")){
+                    cmd = message.getText().toString().split("/")[1].split(" ")[0];
+                    arg = rmsg.split("/say")[1];
+
+                    if(cmd.equals("say")){
+                        sendMessage(arg);
+                    }
+
+
+
+                }
+            }
         }
     }
-
     @Override
     public String getBotUsername() {
         return "intellij_bot";
@@ -32,5 +62,16 @@ public class BotHandler extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return "508112557:AAFOC-R-KXsFniSQSVmGOBT4iSRMWMx2URA";
+    }
+
+    public void sendMessage(String args){
+        SendMessage sendMessageRequest = new SendMessage();
+        sendMessageRequest.setChatId(message.getChatId().toString());
+        sendMessageRequest.setText(args);
+        try {
+            execute(sendMessageRequest);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
