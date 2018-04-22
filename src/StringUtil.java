863ddb37a6b523c81bcaaf15946a7b03ee156467
oscar0812 <oscar0812torres@gmail.com>
2018-04-21 20:28:24 -0500

    other files

--- src/StringUtil.java
+++ src/StringUtil.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class StringUtil {
+    static String charToStr(char[] arr) {
+
+        StringBuilder all = new StringBuilder();
+        for (char anArr : arr) {
+            all.append(anArr);
+        }
+
+        return all.toString();
+    }
 }
