db4b0170eef94e9ee5135fcf6d31ac8bfef2886f
LLLBitLLL <cwmversion4@gmail.com>
2018-04-24 10:50:53 -0700

    Still adding images u tard

--- src/com/bit/telebot/game/ImageGuess.java
+++ src/com/bit/telebot/game/ImageGuess.java
@@ -50,10 +50,19 @@ class ImageGuess{
 
         l.add(new ImageGuess("https://logorealm.com/nike-logo/", "nike"));
 
+        //sport teams
+        l.add(new ImageGuess("https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.amazonaws.com%2Ffreebiesupply%2Flarge%2F2x%2Farizona-cardinals-logo-transparent.png&imgrefurl=https%3A%2F%2Ffreebiesupply.com%2Flogos%2Farizona-cardinals-logo%2F&docid=FcIQuol-ALKNtM&tbnid=nv0Mk64m21fmHM%3A&vet=10ahUKEwjGkLDHuNPaAhUKPN8KHc3sDzwQMwjRASgBMAE..i&w=2400&h=1981&bih=575&biw=637&q=cardinals%20logo&ved=0ahUKEwjGkLDHuNPaAhUKPN8KHc3sDzwQMwjRASgBMAE&iact=mrc&uact=8", "cardinals"));
+        l.add(new ImageGuess("https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.amazonaws.com%2Ffreebiesupply%2Flarge%2F2x%2Fatlanta-falcons-logo-transparent.png&imgrefurl=https%3A%2F%2Ffreebiesupply.com%2Flogos%2Fatlanta-falcons-logo%2F&docid=TZIeWf-i6zdGXM&tbnid=SLRhLbTF5-d7uM%3A&vet=10ahUKEwj1x_jpuNPaAhUPVd8KHYO6C8UQMwisASgAMAA..i&w=2400&h=2289&bih=575&biw=637&q=falcons%20logo&ved=0ahUKEwj1x_jpuNPaAhUPVd8KHYO6C8UQMwisASgAMAA&iact=mrc&uact=8","falcons"));
+        l.add(new ImageGuess("https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.amazonaws.com%2Ffreebiesupply%2Flarge%2F2x%2Fbaltimore-ravens-logo-transparent.png&imgrefurl=https%3A%2F%2Ffreebiesupply.com%2Flogos%2Fbaltimore-ravens-logo%2F&docid=SHxjqa0GUhXVxM&tbnid=G5FT505s9DrzIM%3A&vet=10ahUKEwjc5p20udPaAhXvmuAKHXNRCNcQMwiBAigAMAA..i&w=2400&h=1480&bih=575&biw=637&q=ravens%20logo&ved=0ahUKEwjc5p20udPaAhXvmuAKHXNRCNcQMwiBAigAMAA&iact=mrc&uact=8", "ravens"));
+        l.add(new ImageGuess("https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.amazonaws.com%2Ffreebiesupply%2Flarge%2F2x%2Fbuffalo-bills-logo-transparent.png&imgrefurl=https%3A%2F%2Ffreebiesupply.com%2Flogos%2Fbuffalo-bills-logo%2F&docid=yDIKHl5RRffYiM&tbnid=Ut3lghJVtL4QtM%3A&vet=10ahUKEwi4o7mJutPaAhXKm-AKHUueA9kQMwjFASgAMAA..i&w=2400&h=1700&bih=575&biw=637&q=bills%20logo&ved=0ahUKEwi4o7mJutPaAhXKm-AKHUueA9kQMwjFASgAMAA&iact=mrc&uact=8", "bills"));
+        l.add(new ImageGuess("https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FMK16FBfuQxk%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DMK16FBfuQxk&docid=N7-jngDjuF8tnM&tbnid=YfEonzTWzTuxgM%3A&vet=10ahUKEwjr2amrutPaAhUxhOAKHQKhC3YQMwiqASgAMAA..i&w=1280&h=720&bih=575&biw=637&q=panthers%20logo&ved=0ahUKEwjr2amrutPaAhUxhOAKHQKhC3YQMwiqASgAMAA&iact=mrc&uact=8","panthers"));
+
+
+
 
     }
 
-    static ImageGuess random(){
+    static ImageGuess random(){https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.amazonaws.com%2Ffreebiesupply%2Flarge%2F2x%2Fatlanta-falcons-logo-transparent.png&imgrefurl=https%3A%2F%2Ffreebiesupply.com%2Flogos%2Fatlanta-falcons-logo%2F&docid=TZIeWf-i6zdGXM&tbnid=SLRhLbTF5-d7uM%3A&vet=10ahUKEwj1x_jpuNPaAhUPVd8KHYO6C8UQMwisASgAMAA..i&w=2400&h=2289&bih=575&biw=637&q=falcons%20logo&ved=0ahUKEwj1x_jpuNPaAhUPVd8KHYO6C8UQMwisASgAMAA&iact=mrc&uact=8
         if(l.isEmpty())
             initList();
         return l.get(l.size()-1);
